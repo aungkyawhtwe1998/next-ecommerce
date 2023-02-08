bc5efb889e3c5984f3098248009c5480c7d8142d
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2023-02-08 17:13:50 +0630

    next config edit redo

--- lib/url.ts
+++ lib/url.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASE,
     headers:{
         "Content-Type":"application/json",
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     }
 });
  
--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-  
-// }
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
-module.exports = {
-  async headers() {
-    return [
-      {
-        reactStrictMode: true,
-        source: "/.*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://next-ecommerce-flame.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
+module.exports = nextConfig
