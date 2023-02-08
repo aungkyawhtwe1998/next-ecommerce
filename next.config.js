52e30fa2cff90712974ba82dd48944f55b7f1dc8
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2023-02-08 16:44:35 +0630

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+  
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
